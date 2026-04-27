d4843ec6ce20ebd361d88bf1bdac783a990631d9
D4rkxv <112972404+D4rkxv@users.noreply.github.com>
2026-04-27 10:14:20 +0200

    Update next.config.ts

--- smartload/next.config.ts
+++ smartload/next.config.ts
@@ -1,15 +1,17 @@
 import type { NextConfig } from "next";
 
+const BACKEND_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
         source: "/api/backend/:path*",
-        destination: "http://localhost:8000/:path*",
+        destination: `${BACKEND_URL}/:path*`,
       },
       {
         source: "/static/:path*",
-        destination: "http://localhost:8000/static/:path*",
+        destination: `${BACKEND_URL}/static/:path*`,
       },
     ];
   },
